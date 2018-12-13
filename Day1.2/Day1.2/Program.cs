ab16d24ac0bdd58ba896fd32396795903cb42628
Andra <andrapetersone@gmail.com>
2018-12-13 21:27:21 +0200

    Pareizā ievade

---
+++ Day1.2/Day1.2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Day1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PirmaisUzdevums();
+            /* Console.WriteLine(Piemers());
+             int a = Piemers();
+
+             Console.WriteLine(Piemers2(3, a));
+             Console.ReadLine();
+             */
+            Console.WriteLine("Ievadiet skaitli");
+            String ievade = Console.ReadLine();
+            int skaitlis1 = Convert.ToInt16(ievade);
+
+            Console.WriteLine("Ievadiet skaitli");
+            ievade = Console.ReadLine();
+
+            int skaitlis2 = Convert.ToInt16(ievade);
+
+            int result = SaskaitītVaiAtņemt(skaitlis1, skaitlis2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+
+        }
+
+        static void PirmaisUzdevums()
+        {
+            Console.WriteLine("Ka jus sauc?");
+
+            String vards = Console.ReadLine();
+
+            Console.WriteLine("Sveiki, " + vards);
+        }
+
+        static int Piemers()
+        {
+            return 4 + 3;
+        }
+
+        static int Piemers2(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Piemers3()
+        {
+            Console.WriteLine("Ievadiet simbolus!");
+            String ievade = Console.ReadLine();
+            int a = 5;
+            if (ievade == "vii" && a > 5)
+            {
+                Console.WriteLine("1");
+            }
+            else if (ievade == "vii" && a == 5)
+            {
+                Console.WriteLine("2");
+            }
+            else
+            {
+
+            }
+            // <
+            // >
+            // <=
+            // >=
+            // ==
+            // !=
+
+            // &&
+            // ||
+        }
+        static int SaskaitītVaiAtņemt(int a, int b)
+        {
+            Console.WriteLine("Saskaitīt vai atņemt");
+            String izvēle = Console.ReadLine();
+            if (izvēle == "+")
+            {
+                return a + b;
+            }
+            else if (izvēle == "-") 
+            {
+                return a - b;
+            } 
+            else 
+            {
+                return -99999;
+            }
+
+        }  
+        //funkcija, kas lauj cilvekam izveleties vai saskaita vai atnem
+        //return, gan parametriem
+
+        
+}
+}
