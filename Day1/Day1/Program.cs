67f53ea78d128de10b1717e45376b20c5bdcac8b
Andra <andrapetersone@gmail.com>
2018-12-13 19:52:18 +0200

    Izveidots pirmais projekts un uzdevums

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kā Jūs sauc!");
+            String vārds = Console.ReadLine();
+
+            Console.WriteLine("Sveiki, " + vārds);
+
+            // int, double, String, bool..
+
+            Console.ReadLine();
+
+        }
+    }
+}
