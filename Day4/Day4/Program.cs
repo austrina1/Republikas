b0c02d7bf36841e8d96be67f71b68c531985a6b5
Andra <andrapetersone@gmail.com>
2018-12-18 19:41:32 +0200

    Masīva garuma pieprasīšana

---
+++ Day4/Day4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzdevumi uzd = new Uzdevumi();
+            uzd.PirmaisUzd();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day4/Day4/Uzdevumi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Day4
+{
+    public class Uzdevumi
+    {
+        public Uzdevumi()
+        {
+            
+        }
+
+        public void PirmaisUzd() 
+        {
+            Console.WriteLine("Ievadiet masīva garumu");
+            int garums = Convert.ToInt16(Console.ReadLine());
+
+
+            int[] skaitluMasivs = new int[garums];
+
+            for( int i = 0; i < skaitluMasivs.Length; i++)
+            {
+                skaitluMasivs[i] = i + 2;
+                Console.Write(skaitluMasivs[i] + " ");
+            }
+        }
+    }
+}
