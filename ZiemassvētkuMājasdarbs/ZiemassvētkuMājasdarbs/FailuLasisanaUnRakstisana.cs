364dcbeb9be1a9843a8df6186204cd7925807ef4
Andra <andrapetersone@gmail.com>
2018-12-27 13:37:36 +0200

    Mājasdarbs

---
+++ ZiemassvētkuMājasdarbs/ZiemassvētkuMājasdarbs/FailuLasisanaUnRakstisana.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace ZiemassvētkuMājasdarbs
+{
+    public class FailuLasisanaUnRakstisana
+    {
+     
+
+        public void LasitUnApmainīt()
+        {
+            //ielasam rindas no faila masiva (ta ka piemera)
+            //tas bus string tipa masivs, konvertejam uz int
+
+            string[] lines = File.ReadAllLines(@"dzejolis.txt");
+
+            // Display the file contents by using a foreach loop.
+
+
+            System.Console.WriteLine("Dzejolītis:");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                Console.WriteLine("\t" + line);
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ ZiemassvētkuMājasdarbs/ZiemassvētkuMājasdarbs/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ZiemassvētkuMājasdarbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FailuLasisanaUnRakstisana fails = new FailuLasisanaUnRakstisana();
+            fails.LasitUnApmainīt();
+        }
+    }
+}
